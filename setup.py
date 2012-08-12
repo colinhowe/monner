c2837ca5d91ea6636853540a58c51291cbed401c
Colin Howe <colinthehowe@gmail.com>
2012-08-12 18:33:26 +0100

    Tabs...

--- setup.py
+++ setup.py
@@ -8,25 +8,25 @@ if not root_dir:
 long_desc = open(root_dir + '/README.rst').read()
 
 setup(
-	name='monner',
-	version='0.3.0',
-	description='Monitors the system whilst executing a given program',
-	url='https://github.com/colinhowe/monner',
-	author='Colin Howe',
-	author_email='colin@colinhowe.co.uk',
+    name='monner',
+    version='0.3.0',
+    description='Monitors the system whilst executing a given program',
+    url='https://github.com/colinhowe/monner',
+    author='Colin Howe',
+    author_email='colin@colinhowe.co.uk',
     scripts=['monner.py'],
     install_requires=['psutil >= 0.4.0'],
-	classifiers=[
-		'Development Status :: 4 - Beta',
+    classifiers=[
+        'Development Status :: 4 - Beta',
         'License :: OSI Approved :: Apache Software License',
-		'Operating System :: OS Independent',
-		'Topic :: Utilities',
-	],
+        'Operating System :: OS Independent',
+        'Topic :: Utilities',
+    ],
     entry_points= {
         'console_scripts': [
             'monner = monner:main'
         ]
     },
-	license='Apache 2.0',
-	long_description=long_desc,
+    license='Apache 2.0',
+    long_description=long_desc,
 )
