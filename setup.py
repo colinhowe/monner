d77bc40c8ca50c19ec37d5dba068cea313c6c7e3
Colin Howe <colinthehowe@gmail.com>
2012-08-12 17:46:48 +0100

    Clarify increment

--- setup.py
+++ setup.py
@@ -9,13 +9,13 @@ long_desc = open(root_dir + '/README.rst').read()
 
 setup(
 	name='monner',
-	version='0.2.0',
+	version='0.2.1',
 	description='Monitors the system whilst executing a given program',
 	url='https://github.com/colinhowe/monner',
 	author='Colin Howe',
 	author_email='colin@colinhowe.co.uk',
     scripts=['monner.py'],
-    install_requires=['psutil >= 0.4'],
+    install_requires=['psutil >= 0.4.0'],
 	classifiers=[
 		'Development Status :: 4 - Beta',
         'License :: OSI Approved :: Apache Software License',
